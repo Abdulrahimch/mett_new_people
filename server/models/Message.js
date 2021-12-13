c8d8307b1318f6c2ca18a0c4b4b91d087af4d548
abdulraim <abdulrahimcisco92@gmail.com>
2021-12-13 19:03:50 +0300

    message model

---
+++ server/models/Message.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema(
+    {
+        conversation: {
+            type: mongoose.Types.ObjectId,
+            ref: "conversation",
+            required: true,
+        },
+        sender: {
+            type: mongoose.Types.ObjectId,
+            ref: "user",
+            required: true,
+        },
+        text: String
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = Message = mongoose.model("message", userSchema);
+
